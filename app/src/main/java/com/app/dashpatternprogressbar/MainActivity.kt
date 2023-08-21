4ade7b9123f78e9093007bac9d2792de1d9d560b
KudikyalaSaiSharath <sharath.kudikyala@rampgroup.com>
2023-08-21 20:03:02 +0530

    Custom view integrated in activity

--- app/src/main/java/com/app/dashpatternprogressbar/MainActivity.kt
+++ app/src/main/java/com/app/dashpatternprogressbar/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.app.dashpatternprogressbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.app.dashpatternprogressbar.custom.CircularProgressBar
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnSetProgress).setOnClickListener {
+            val progress = Random.nextInt(0, 100)
+            findViewById<TextView>(R.id.tvProgress).text = "$progress/100"
+            findViewById<CircularProgressBar>(R.id.circularProgressBar).animateProgress(progress)
+        }
     }
 }
